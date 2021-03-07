20abec62545e0b8aedded1d6efe7cd88ffd646b9
HamzaOstonov <hamza_ost@mail.ru>
2021-03-07 22:48:55 +0500

    11

--- src/components/Savol.vue
+++ src/components/Savol.vue
@@ -3,61 +3,16 @@
     <strong>Question {{ questionNumber }}:</strong><br />
     <strong>{{ question.Savoltext }} </strong>
 
-    <div v-for="(mcanswer, index) in question.Javoblar" :key="index">
-      <div>
-        <label>{{ mcanswer.Javobtext }}</label>
+      <div v-for="(mcanswer, index) in question.Javoblar" :key="index" class="divjavob">
+        <div class="divjavobcheck">
+          <input type="checkbox" value="Daniel" v-model="namesThatRhyme"/>
+        </div>
+        <div class="divjavobtext">
+          {{mcanswer.Javobnum}}
+          {{mcanswer.Javobtext}}
+        </div>
       </div>
-      <button @click="submitAnswer">{{ mcanswer.Javobnum }}</button>
-      <br />
-    </div>
 
-    <v-list>
-      <div v-for="(mcanswer, index) in question.Javoblar" :key="index">
-        <v-list-item>
-          <v-list-item-action>
-            <v-icon></v-icon>
-          </v-list-item-action>
-
-          <v-list-item-title v-text="mcanswer.Javobtext" />
-        </v-list-item>
-      </div>
-    </v-list>
-
-     <v-card
-    max-width="400"
-  >
-
-    <v-list
-    >
-      <v-subheader>Shoping List:</v-subheader>
-
-      <v-list-item-group multiple v-model="settings" >
-        <v-list-item
-          v-for="(item, index) in question.Javoblar"
-          :key="index"
-          v-model="item.finished"
-        >
-          <template v-slot:default="{ active, toggle }">
-            <v-list-item-action>
-              <v-checkbox
-                :input-value="active"
-                @click="toggle"
-              ></v-checkbox>
-            </v-list-item-action>
-
-            <v-list-item-content>
-              <v-list-item-title :class="{ finished: item.finished }">{{
-                item.Javobtext
-              }}</v-list-item-title>
-              <v-list-item-subtitle :class="{ finished: item.finished }">{{
-                item.Javobnum
-              }}</v-list-item-subtitle>
-            </v-list-item-content>
-          </template>
-        </v-list-item>
-      </v-list-item-group>
-    </v-list>
-    </v-card>
   </div>
 </template>
 <script>
@@ -106,5 +61,19 @@ export default {
 }
 .v-list::-webkit-scrollbar-thumb {
   background-color: coral;
+}
+.divjavob {
+  position: relative;
+  display: block;
+  width: 100%;
+  border-bottom: 1px solid rgb(202, 202, 202);
+  cursor: pointer;
+}
+.divjavobcheck {
+  position: absolute;
+}
+.divjavobtext{
+  text-align: left;
+      margin: 0 0 10px 30px;
 }
 </style>
