3d11a0d072a71225f0e4f473258a59e95a79e32f
HamzaOstonov <hamza_ost@mail.ru>
2021-03-28 21:48:30 +0500

    11

--- src/components/Popup.vue
+++ src/components/Popup.vue
@@ -1,50 +1,83 @@
 <template>
-<v-dialog v-model="show" max-width="500px">
-
-
-      <v-card>
+  <v-dialog v-model="show" max-width="500px">
+    <v-card>
       <v-card-title>
         <h2>Add a New Question-Yangi test savoli kiritish</h2>
       </v-card-title>
       <v-card-text>
         <v-form class="px-3" ref="form">
-          <v-text-field v-model="title" label="Title" prepend-icon="folder" :rules="inputRules"></v-text-field>
-          <v-textarea  auto-grow v-model="content" label="Savol matni" prepend-icon="edit" :rules="inputRules"></v-textarea>
+          <v-textarea
+            auto-grow
+            v-model="content"
+            label="Savol matni"
+            prepend-icon="edit"
+            :rules="inputRules"
+          ></v-textarea>
 
-          <v-menu v-model="menu" :close-on-content-click="false">
-            <v-text-field slot="activator" :rules="inputRules"
-              :value="formattedDate" clearable label="Due date" prepend-icon="date_range">
-            </v-text-field>
-            <v-date-picker v-model="due" @change="menu = false"></v-date-picker>
-          </v-menu>
+          <v-textarea
+            auto-grow
+            v-model="javob"
+            label="javob varianti"
+            prepend-icon="edit"
+            rows="2"
+            :rules="inputRules"
+          ></v-textarea>
+
+          <v-btn tile small color="success">
+            <v-icon dark> mdi-plus </v-icon>
+            Javobni qushib quyish
+          </v-btn>
+
+          <v-radio-group label="" v-model="active">
+            <v-radio
+              v-for="(mcanswer, index) in javoblar"
+              :key="index"
+              name="active"
+              :label="mcanswer.Javobtext"
+              :value="mcanswer.Javobnum"
+              @change="getValue(questionNumber, mcanswer.Javobnum)"
+            >
+            </v-radio>
+          </v-radio-group>
 
           <v-spacer></v-spacer>
 
-          <v-btn flat @click="submit" class="success mx-0 mt-3" :loading="loading">Add Project</v-btn>
+          <v-btn
+            flat
+            @click="submit"
+            class="success mx-0 mt-3"
+            :loading="loading"
+            >Testni bazaga qushib quyish</v-btn
+          >
         </v-form>
       </v-card-text>
-       <v-card-actions>
-      <v-btn color="primary" flat @click.stop="show=false">Close</v-btn>
-    </v-card-actions>
-    </v-card> 
-
-</v-dialog>
+      <v-card-actions>
+        <v-btn color="primary" flat @click.stop="show = false">Close</v-btn>
+      </v-card-actions>
+    </v-card>
+  </v-dialog>
 </template>
 
 <script>
 export default {
-  props: ['visible'],
+  data() {
+    return {
+      javoblar: [{ Javobtext: "Javob number bir" }],
+      javobnum: 1,
+    };
+  },
+  props: ["visible"],
   computed: {
     show: {
-      get () {
-        return this.visible
+      get() {
+        return this.visible;
       },
-      set (value) {
+      set(value) {
         if (!value) {
-          this.$emit('close')
+          this.$emit("close");
         }
-      }
-    }
-  }
-}
+      },
+    },
+  },
+};
 </script>
